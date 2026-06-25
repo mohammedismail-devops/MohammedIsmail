f62fbc238287033e19dba10e784eb03747490e84
Mohammed Ismail A <mi1279550@gmail.com>
2026-06-25 20:36:31 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,1 @@
-const reveals = document.querySelectorAll(".reveal");
 
-const observer = new IntersectionObserver(
-    entries => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add("active");
-                observer.unobserve(entry.target);
-            }
-        });
-    },
-    { threshold: 0.2 }
-);
-
-reveals.forEach(section => {
-    observer.observe(section);
-});
